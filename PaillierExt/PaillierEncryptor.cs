9a00d3b8bd71dd5b4e09f80c522b3c8a7bff7736
bazzilic <bazzilic@gmail.com>
2015-05-28 21:53:25 +0800

    License

--- PaillierExt/PaillierEncryptor.cs
+++ PaillierExt/PaillierEncryptor.cs
@@ -1,4 +1,13 @@
-﻿using System;
+﻿/************************************************************************************
+ This is an implementation of the Paillier encryption scheme with support for
+ homomorphic addition.
+ 
+ This library is provided as-is and is covered by the MIT License [1].
+  
+ [1] The MIT License (MIT), website, (http://opensource.org/licenses/MIT)
+ ************************************************************************************/
+
+using System;
 
 namespace PaillierExt
 {
