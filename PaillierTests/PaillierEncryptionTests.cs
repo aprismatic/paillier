68ef1b23e966fb5d5d48f260333d56546a2909e4
Baron-Chen <baronchen1350@gmail.com>
2018-09-27 13:32:58 +0800

    Added Floating Point Case Test.

--- PaillierTests/PaillierEncryptionTests.cs
+++ PaillierTests/PaillierEncryptionTests.cs
@@ -4,6 +4,7 @@ using System;
 using System.Numerics;
 using System.Security.Cryptography;
 using Xunit;
+using Numerics;
 
 namespace PaillierTests
 {
@@ -174,5 +175,100 @@ namespace PaillierTests
                 Assert.Equal(z + z_2, z_addition);
             }
         }
-    }
+
+		[Fact(DisplayName = "Floating point")]
+		public void TestFloatingPoint()
+		{
+			{
+				var algorithm = new Paillier
+				{
+					KeySize = 384
+				};
+
+				/**
+				 *Test 1 decimal place
+
+				 **/
+
+				var z = new BigRational(new Decimal(0.1));
+
+				//Convert fraction to whole number. Denominator will be the exponent
+				var z_whole = z.Numerator;
+				var z_exponent = z.Denominator;
+
+				//Encrypt and decrypt
+				var z_enc_bytes = algorithm.EncryptData(z_whole);
+				var z_dec = algorithm.DecryptData(z_enc_bytes);
+
+				//Reconvert back to fraction
+				var z_dec_float = new BigRational(z_dec, z_exponent);
+
+				Assert.Equal(z, z_dec_float);
+
+
+
+
+				/**
+				 *Test 2 decimal places
+
+				 **/
+
+				var z_2 = new BigRational(new Decimal(0.02));
+
+				//Convert fraction to whole number. Denominator will be the exponent
+				var z_2_whole = z_2.Numerator;
+				var z_2_exponent = z_2.Denominator;
+
+				//Encrypt and decrypt
+				var z_enc_bytes_2 = algorithm.EncryptData(z_2_whole);
+				var z_dec_2 = algorithm.DecryptData(z_enc_bytes_2);
+
+				//Reconvert back to fraction
+				var z_dec_2_float = new BigRational(z_dec_2, z_2_exponent);
+
+				Assert.Equal(z_2, z_dec_2_float);
+
+
+
+				/**
+                 *  Test addition of 2 floats with different decimal places
+                 * */
+
+				z = new BigRational(new Decimal(0.1));
+				z_2 = new BigRational(new Decimal(0.02));
+
+				//For addition, the exponent of both plain text needs to be the same
+				var common_exponent = new BigInteger(1);
+
+				//The larger exponent among the 2 floats will be used as the common exponent
+				if (z.Denominator.CompareTo(z_2.Denominator) == 1)
+				{
+					common_exponent = z.Denominator;
+				}
+				else
+				{
+					common_exponent = z_2.Denominator;
+				}
+
+				//Convert fraction to whole number
+				z_whole = z.Numerator * (common_exponent / z.Denominator);
+				z_2_whole = z_2.Numerator * (common_exponent / z_2.Denominator);
+
+				//Encrypt
+				z_enc_bytes = algorithm.EncryptData(z_whole);
+				z_enc_bytes_2 = algorithm.EncryptData(z_2_whole);
+
+				//Addition
+				var z_enc_addition = algorithm.Addition(z_enc_bytes, z_enc_bytes_2);
+
+				//Decrypt
+				var z_addition = algorithm.DecryptData(z_enc_addition);
+
+				//Convert to fraction
+				var z_addition_fraction = new BigRational(z_addition, common_exponent);
+
+				Assert.Equal(z + z_2, z_addition_fraction);
+			}
+		}
+	}
 }
